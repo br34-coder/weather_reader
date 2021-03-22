ef4a99201912a05b3487ea40e75faba9e285c26c
Marco Wagner <marco.andreas.wagner@gmail.com>
2021-03-22 14:17:35 +0100

    added telegram send service including docs in readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # weather_reader
-Little playground to read data from my weather station on wunderground
+Little playground to read data from my weather station and post it via telegram messenger
+
+## How to init the telegram sender
+This section describes how to create a bot in telegram and use it to send messages via python
+
+### Create the bot
+1. Open the telegram app on your smartphone and search for @BotFather
+2. Press the start button
+3. Send "/newbot", create a name and username
+4. Note down the API token provided
+
+### Prepare telegram-send
+1. Install telegram-send: `pip3 install telegram-send`
+2. Configure telegram-send: `telegram-send --configure`
+3. The config tool will now ask you for the token you got from the BotFather, ask you to add him on your telegram App and send a password given.
+
+### Send messages using python
+Once the preps have been done, its straight forward:
+```python
+import telegram_send
+telegram_send.send(messages=["Hello World!"])
+``` 
--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 
-# Simple python tool to retrieve weather station information and parse the JSON for data
-# requires: requests, json
+# Simple python tool to retrieve weather station information and parse the JSON for data and send to telegram
+# requires: requests, json, telegram_send
 import requests
 import json
+import telegram_send
 
-# Retrieve the data as a JSON
-data_json = requests.get("https://api.weather.com/v2/pws/observations/current?stationId=ITALHE7&format=json&units=m&apiKey=6532d6454b8aa370768e63d6ba5a832e").text
+# send info via telegram
+def send_data(text):
+    telegram_send.send(messages=[text])
 
-# Transform into a dict (actually a dict of list of dict of dict)
-data_dict = json.loads(data_json)
+# retrieve and transform the weather data as a dict
+def get_data(link):
+    # Retrieve the data as a JSON
+    data_json = requests.get(link).text
 
+    # Transform into a dict (actually a dict of list of dict of dict)
+    data_dict = json.loads(data_json)
+
+    return data_dict
+
+# retrieve
+data = get_data("https://api.weather.com/v2/pws/observations/current?stationId=ITALHE7&format=json&units=m&apiKey=6532d6454b8aa370768e63d6ba5a832e")
+
+message = "Current weather in Talheim, DE: \n"
+message += "Timestamp: " + data["observations"][0]["obsTimeLocal"] + "\n"
+message += "Temperature (°C): " + str(data["observations"][0]["metric"]["temp"]) + "\n"
+message += "Wind Speed (km/h): " + str(data["observations"][0]["metric"]["windSpeed"]) + "\n"
+send_data(message)
 # Play around with the data
 
 # Pretty print
-#print(json.dumps(data_dict, indent = 4, sort_keys=True))
+#print(json.dumps(data, indent = 4, sort_keys=True))
 
 # Print single values
-print("Timestamp: ",data_dict["observations"][0]["obsTimeLocal"])
-print("Temperature (°C): ",data_dict["observations"][0]["metric"]["temp"])
-print("Wind Speed (km/h): ",data_dict["observations"][0]["metric"]["windSpeed"])
+#print("Timestamp: ",data["observations"][0]["obsTimeLocal"])
+#print("Temperature (°C): ",data["observations"][0]["metric"]["temp"])
+#print("Wind Speed (km/h): ",data["observations"][0]["metric"]["windSpeed"])
